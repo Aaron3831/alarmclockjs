5571de87e2f2756997b3c58537b184c0e41785e8
Aaron Nguyen and Dan Jacob <dan@>
2017-05-23 11:50:01 -0700

    almost done, but not really

--- js/alarm-interface.js
+++ js/alarm-interface.js
@@ -1,3 +1,31 @@
 $(document).ready(function(){
-  $('#time').text(moment());
+  $("#alarm-clock-form").submit(function(event){
+    event.preventDefault();
+    var alarmset = Moment().hour(hours).minute(minutes)
+  })
+});
+
+
+$(document).ready(function(){
+  $("#alarm").submit(function(event){
+    event.preventDefault();
+    var alarm = $('#alarm').val();
+    var alarmClock = new Moment;
+  })
+});
+
+var Whatever = require('./../js/alarm.js').alarmClock;
+
+// var datetime = null,
+//         date = null;
+
+var update = function () {
+    date = moment(new Date())
+    datetime.html(date.format('h:mm:ss a'));
+};
+
+$(document).ready(function(){
+    datetime = $('#time')
+    update();
+    setInterval(update, 1000);
 });
---
+++ js/alarm.js
@@ -0,0 +1,10 @@
+function Moment(hour, minute){
+  this.hour = hour;
+  this.minute = minute;
+}
+
+Moment.prototype.alarm = function (alarm) {
+
+};
+
+exports.alarmClock = Moment;
